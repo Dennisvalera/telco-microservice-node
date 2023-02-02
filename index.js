4facf097b941801e5297f7b9e4c2a8ccda97bfb7
Dennis Valera <dvalerag@emeal.nttdata.com>
2023-02-02 14:28:36 +0100

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+var path = require("path");
+const express = require("express");
+var cors = require("cors");
+var app = express();
+var fs = require("fs");
+
+app.use(cors());
+
+app.get("/getDictionary", (req, res) => {
+  const module = req.query["module"];
+  const lang = req.query["lang"];
+
+  try {
+    if (module) {
+      const jsonFile = fs.readFileSync(
+        path.join(__dirname, `/assets/i18n/${module}/${lang}.json`)
+      );
+      const jsonDictionary = JSON.parse(jsonFile);
+      res.send(jsonDictionary);
+    } else {
+      const jsonFile = fs.readFileSync(
+        path.join(__dirname, `/assets/i18n/${lang}.json`)
+      );
+      const jsonDictionary = JSON.parse(jsonFile);
+      res.send(jsonDictionary);
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("listening on port", 3000);
+});
