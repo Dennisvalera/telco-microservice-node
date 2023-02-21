419c637990f7ba90490811f94d3a5981f9bff85e
Nadia <nalleral@nttdata.com>
2023-02-21 15:44:10 +0100

    FEAT: add env configs, refactor i18n

--- index.js
+++ index.js
@@ -6,19 +6,34 @@ var fs = require("fs");
 
 app.use(cors());
 
-app.get("/getDictionary", (req, res) => {
-  const module = req.query["module"];
-  const lang = req.query["lang"];
-
+function sendFileContent(filePath) {
   try {
-    const folder = module ? `/assets/i18n/${module}/${lang}.json` : `/assets/i18n/${lang}.json`;
-    const file = fs.readFileSync(path.join(__dirname, folder));
-    const dictionry = JSON.parse(file);
-    res.send(dictionry);
+    const file = fs.readFileSync(path.join(__dirname, filePath));
+    const content = JSON.parse(file);
+    res.send(content);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
+}
+
+app.get("/getConfig", (req, res) => {
+  const env = req.query["env"];
+  const filePath = `/config/${env}/config.json`;
+  sendFileContent(filePath);
 });
+
+app.get("/getConfigI18n", (req, res) => {
+  const filePath = `/i18n/config.json`;
+  sendFileContent(filePath);
+});
+
+app.get("/getDictionary", (req, res) => {
+  const module = req.query["module"];
+  const lang = req.query["lang"];
+  const filePath = module ? `/i18n/assets/${module}/${lang}.json` : `/i18n/assets/${lang}.json`;
+  sendFileContent(filePath);
+});
+
 
 app.listen(3000, () => {
   console.log("listening on port", 3000);
